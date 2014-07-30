ad16fb0d70b87f18a22d5c007ab96717562dbd84
Mike Schneider <schneider707@gmail.com>
2014-07-29 17:19:30 -0700

    Slideshow effect now works.

--- js/file-api-slideshow.js
+++ js/file-api-slideshow.js
@@ -21,38 +21,50 @@ window.addEvent('domready', function() {
         return false;
     };
     holder.ondrop = function (e) {
+        var interval = 5000; //time between next heatmap in ms.
         this.className = '';
         e.preventDefault();
 
         var reader = new FileReader();
         var files = e.dataTransfer.files;
-        for(var i = 0; files.length; i++) {
-            var file = files[i]; 
+        if(files.length == 1){
+            var file = files[0]; 
             reader.readAsText(file);
             reader.onload = function (event) {
-                if (mapLoaded == false) { //load map
-                    svgfile = event.target.result;
-                    tempWidth = $(svgfile)[6].viewBox.baseVal.width;
-                    tempHeight = $(svgfile)[6].viewBox.baseVal.height;
-                    map.style.width = tempWidth;
-                    map.style.height = tempHeight;
-                    display_map(file.name, tempWidth, tempHeight);
-                    add_heatmap();
-                    mapLoaded = true;
-                    state.innerHTML = 'And now your heatmaps!';
-                }
-                else { //load heatmap
-                    csvfile = event.target.result;
-                    //csvfile = event.target.result;
-                    csvArray = $.csv.toArrays(csvfile);
-                    for
-                    populate_heatmap(csvArray, minHeatValue, maxHeatValue);
-                    save_heatmap('jpeg', csvfile);
-                }
-
+                svgfile = event.target.result;
+                tempWidth = $(svgfile)[6].viewBox.baseVal.width;
+                tempHeight = $(svgfile)[6].viewBox.baseVal.height;
+                map.style.width = tempWidth;
+                map.style.height = tempHeight;
+                display_map(file.name, tempWidth, tempHeight);
+                add_heatmap();
+                mapLoaded = true;
+                state.innerHTML = 'And now your heatmaps!';
             };
-
         }
+        else      
+        {
+            var i = 0;
+            var intervalHandle = setInterval(function() {
+                processCSV();
+                i = i + 1;
+                if(files.length == i){
+                    clearInterval(intervalHandle);
+                }
+            }, interval);
+            
+            function processCSV(){
+                var file = files[i];
+                console.log(file);
+                reader.readAsText(file);
+                reader.onload = function (event) {
+                    csvfile = event.target.result;
+                    csvArray = $.csv.toArrays(csvfile);
+                    populate_heatmap(csvArray, 0.05, 1.0);
+                };
+            }
+        }
+
         return false;
     };
 });
