094a09e220409ac5f86c04664258b067865a22c5
Mike Schneider <schneider707@gmail.com>
2014-07-31 16:32:23 -0700

    Cleanup

--- js/map-function.js
+++ js/map-function.js
@@ -123,7 +123,7 @@ function display_map(map_url, width, height) {
             new OpenLayers.Control.PanZoomBar(),
                 ],
         maxExtent: bounds,
-        numZoomLevels: 8,
+        numZoomLevels: 10,
         fractionalZoom: true,
     }
     map = new OpenLayers.Map ("map", map_options);
