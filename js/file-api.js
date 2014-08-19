e3839dc76f0862835a0bc02145ed571dbc64d84b
Mike Schneider <schneider707@gmail.com>
2014-08-19 15:20:33 -0700

    Merged slideshow functionality into main files.

--- js/file-api.js
+++ js/file-api.js
@@ -1,4 +1,4 @@
-// modified from http://html5demos.com/file-api
+var reader;
 
 window.addEvent('domready', function() {
     var holder = document.getElementById('holder'),
@@ -23,37 +23,68 @@ window.addEvent('domready', function() {
         return false;
     };
     holder.ondrop = function (e) {
+        var interval = 1500; //time between next heatmap in ms.
         this.className = '';
         e.preventDefault();
-        var minHeatValue = parseFloat(document.getElementById("minHeatValue").value);
-        var maxHeatValue = parseFloat(document.getElementById("maxHeatValue").value);
-        //var intensityScalar = document.getElementById("scalar").value;
 
-        var file = e.dataTransfer.files[0],
-            reader = new FileReader();
-        reader.onload = function (event) {
+        var canvas;
+        var heat_div = document.createElement('div');
+        heat_div.id = "heat-title";
+        var heat_title;
 
-            if (mapLoaded == false) { //load map
+        reader = new FileReader();
+        var files = e.dataTransfer.files;
+        var file = files[0];
+        var extension = file.name.split('.').pop();
+        if(files.length == 1 && extension == "svg"){
+            reader.readAsText(file);
+            reader.onload = function (event) {
                 svgfile = event.target.result;
+
                 tempWidth = $(svgfile)[6].viewBox.baseVal.width;
                 tempHeight = $(svgfile)[6].viewBox.baseVal.height;
                 map.style.width = tempWidth;
                 map.style.height = tempHeight;
-                display_map(file.name, tempWidth, tempHeight);
-                add_heatmap();
-                mapLoaded = true;
-                state.innerHTML = 'And now your heatmap!';
-            }
-            else { //load heatmap
-                csvfile = event.target.result;
-                csvArray = $.csv.toArrays(csvfile);
-                populate_heatmap(csvArray, minHeatValue, maxHeatValue);
-                //populate_heatmap(csvArray, csvArray.length, intensityScalar, minHeatValue, maxHeatValue);
-            }
 
-        };
-        reader.readAsText(file);
+                display_map(file.name, tempWidth, tempHeight);
+
+                add_heatmap();
+
+                canvas = document.getElementsByTagName("canvas")[0];
+                canvas.parentNode.appendChild(heat_div);
+
+                mapLoaded = true;
+                state.innerHTML = 'And now your heatmaps!';
+            };
+        }
+        // show single heatmap
+        else if(files.length == 1 && extension == "csv"){
+            processCSV(files[0]);    
+        }
+        // show "slideshow" of heatmaps
+        else
+        {
+            var i = 0;
+            var intervalHandle = setInterval(function() {
+                processCSV(files[i]);
+                i = i + 1;
+                if(files.length == i){
+                    clearInterval(intervalHandle);
+                }
+            }, interval);
+        }
 
         return false;
     };
 });
+
+function processCSV(file){
+    reader.readAsText(file);
+    reader.onload = function (event) {
+        var csvfile = event.target.result;
+        csvArray = $.csv.toArrays(csvfile);
+        heat_title = document.getElementById("heat-title");
+        heat_title.innerHTML = file.name.split(".")[0];
+        populate_heatmap(csvArray, 0.05, 1.0);
+    };
+}
